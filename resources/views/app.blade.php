6123f796791278c8caa802bf6f46e69db27311c2
davidruiz <clarkddr@gmail.com>
2026-03-31 13:30:28 -0700

    se arreglo el viewport

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,18 +2,20 @@
 <html lang="es">
 <head>
     <meta charset="utf-8">
-    <!-- CSS DataTables -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
     <link rel="stylesheet"
           href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
 
-    <!-- jQuery primero -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-
-    <!-- Luego DataTables -->
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-    @vite('resources/js/app.js')
+
+    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
 </head>
-<body class="antialiased">
+<body class="font-sans antialiased bg-gray-900 text-gray-100">
 @inertia
 </body>
 </html>
